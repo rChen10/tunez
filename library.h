d0556c41305a6c76b3b6b5b61b5403e6c73e2fec
Ricky Chen <rchen10@stuy.edu>
2017-10-19 11:48:48 -0400

    Added function defs

--- library.h
+++ library.h
@@ -1,5 +1,14 @@
 struct song_node * table[26];
-struct song_node* add(char *new_name, char *new_artist);
+struct song_node *add(char *new_name, char *new_artist);
+struct song_node *search_sa(char *new_name, char *new_artist);
+struct song_node *search_a(char *new_artist);
+void print_letter(char letter);
+void print_artist(char *new_artist);
+void print_lib();
+void shuffle();
+void  remove(char *song_name);
+void clear();
+
 
 
 
--- list.h
+++ list.h
@@ -5,5 +5,6 @@ struct song_node{
 };
 
 void print_list(struct song_node*);
-struct song_node * insert_front(struct song_node *,char*,char*);
-struct song_node * free_list(struct song_node *);
+struct song_node *insert_front(struct song_node *,char*,char*);
+struct song_node *insert_order(struct song_node *,char*,char*);
+struct song_node *free_list(struct song_node *);
