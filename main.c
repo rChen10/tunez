14c664e50f3b3df72e215ad2669913f2bf44cb5c
Ricky Chen <rchen10@stuy.edu>
2017-10-19 23:27:04 -0400

    redid main

--- main.c
+++ main.c
@@ -3,34 +3,16 @@
 #include "list.h"
 #include "library.h"
 int main(){
-  //int i;
-  //for(i = 0;i < 26;i++){table[i]=NULL;} //init table
-	/*add("hats","1");
-	add("cats","1");
-	add("cookies","1");
-	add("fish","2");
-	add("fried fish","3");
-	add("butter","3");
-	add("butter","2");*/
-	//print_library();
-
-	struct song_node *head = insert_front(0, "1", "apples");
-	print_list(head);
-	insert_order(head,"1","hats");
-	print_list(head);
-	insert_order(head,"1","cats");
-	print_list(head);
-	insert_order(head,"1","cookies");
-	print_list(head);
-	insert_order(head,"2","cookies");
-	print_list(head);
-	insert_order(head,"3","fried fish");
-	print_list(head);
-	insert_order(head,"3","butter");
-	print_list(head);
-	insert_order(head,"2","butter");
-	print_list(head);
-	lremove(head, "1", "cats");
-	print_list(head);
+  struct song_node *Q = insert_front(0, "brighton rock", "queen");
+  insert_order(Q, "good company", "queen");
+  insert_order(Q, "wish you were here", "pink floyd");
+  insert_order(Q, "the man who sold the world", "david bowie");
+  insert_order(Q, "the man who sold the world", "nirvana");
+  insert_order(Q, "comfortably numb", "pink floyd");
+  
+  printf("LINKED LIST TESTS\n");
+  printf("=====================\n");
+  printf("Testing print_list:\n");
+  print_list(Q);
 	
 }
